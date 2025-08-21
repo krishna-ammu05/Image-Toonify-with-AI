fae302923244a9bb163f5d907e00cfe8e51b5e7d
BingiUmesh <bingiumesh.13@gmail.com>
2025-08-21 17:34:08 +0530

    passport

--- Cartoon/app.js
+++ Cartoon/app.js
@@ -1,30 +1,27 @@
 const express = require("express");
 const app = express();
-const ejsMate = require('ejs-mate');
-const path = require("path")
+const ejsMate = require("ejs-mate");
+const path = require("path");
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,"views/"));
-app.use(express.static(path.join(__dirname,"public/js")));
-app.use(express.static(path.join(__dirname,"public/css")));
-app.use(express.static(path.join(__dirname,"public/images")));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views/"));
+app.use(express.static(path.join(__dirname, "public/js")));
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/images")));
 
+app.get("/", (req, res) => {
+  res.render("Home.ejs");
+});
 
+app.get("/auth/login", (req, res) => {
+  res.render("auth/Login.ejs");
+});
 
-app.get("/",(req,res)=>{
-    res.render("Home.ejs");
-})
+app.get("/auth/register", (req, res) => {
+  res.render("auth/Register.ejs");
+});
 
-app.get("/auth/login",(req,res)=>{
-    res.render("auth/Login.ejs");
-})
-
-app.get("/auth/register",(req,res)=>{
-    res.render("auth/Register.ejs");
-})
-
-
-app.listen(3000,()=>{
-    console.log("Server is running at port 3000");
-})
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
